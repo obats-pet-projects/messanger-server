5e322e318a4a58c0f80692b43acb646e5d47fd10
Orest Bats <orest.bats@gmail.com>
2019-05-29 15:08:43 +0300

    Closes #1 Edit try/catch statement

--- api/controllers/messages-controller.js
+++ api/controllers/messages-controller.js
@@ -1,30 +1,24 @@
 const Message = require('../models/message-model');
 
 const create = (req, res) => {
-  try {
-    const { title, message } = req.body;
+  const { title, message } = req.body;
 
-    if (!title || !message) {
-      return res.status(422).send('Please, fill up all fields!');
-    }
-
-    Message.create({
-      title,
-      message
-    })
-      .then(() => res.status(200).send('Messages sent'))
-      .catch(error => res.status(422).send(error));
-  } catch (error) {
-    res.sendStatus(500);
+  if (!title || !message) {
+    return res.status(422).send('Please, fill up all fields!');
   }
+
+  Message.create({
+    title,
+    message
+  })
+    .then(() => res.status(200).send('Message sent'))
+    .catch(error => res.status(500).send(error));
 };
 
 const getAll = (req, res) => {
-  try {
-    Message.findAll().then(message => res.status(200).send(message));
-  } catch (error) {
-    res.sendStatus(500);
-  }
+  Message.findAll()
+    .then(message => res.status(200).send(message))
+    .catch(error => res.status(500).send(error));
 };
 
 module.exports = { create, getAll };
