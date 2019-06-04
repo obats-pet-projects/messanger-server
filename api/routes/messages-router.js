5230b20e0abeec85658ae9357e36efba087ea735
Orest Bats <orest.bats@gmail.com>
2019-06-04 16:24:13 +0300

    Add get one message method

--- api/routes/messages-router.js
+++ api/routes/messages-router.js
@@ -3,7 +3,8 @@ const messagesController = require('../controllers/messages-controller');
 
 const router = new express.Router();
 
-router.get('/:category', messagesController.getByCategory);
+router.get('/:id', messagesController.getOne);
+router.get('/category/:category', messagesController.getByCategory);
 router.post('/', messagesController.create);
 router.delete('/', messagesController.deleteAll);
 router.delete('/:id', messagesController.deleteOne);
