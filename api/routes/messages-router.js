93c554077a83ba074172160863ec7752abe19bb3
Orest Bats <orest.bats@gmail.com>
2019-06-07 18:27:45 +0300

    Add desc sorting for GET messages

--- api/routes/messages-router.js
+++ api/routes/messages-router.js
@@ -4,8 +4,9 @@ const messagesController = require('../controllers/messages-controller');
 const router = new express.Router();
 
 router.get('/:id', messagesController.getOne);
-router.get('/category/:category', messagesController.getByCategory);
+router.get('/mail/category', messagesController.getByCategory);
 router.post('/', messagesController.create);
+router.patch('/', messagesController.updateCategory);
 router.delete('/', messagesController.deleteAll);
 router.delete('/:id', messagesController.deleteOne);
 
